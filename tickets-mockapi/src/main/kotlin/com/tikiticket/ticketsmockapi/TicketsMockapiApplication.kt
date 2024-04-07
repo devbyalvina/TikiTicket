6cc81ec2a24e7f7ff57e55bcb6fb124e8ab78c81
devbyalvina <horannabiii@gmail.com>
2024-04-08 03:24:14 +0900

    Create

---
+++ tickets-mockapi/src/main/kotlin/com/tikiticket/ticketsmockapi/TicketsMockapiApplication.kt
@@ -0,0 +1,11 @@
+package com.tikiticket.ticketsmockapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketsMockapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TicketsMockapiApplication>(*args)
+}
---
+++ tickets-mockapi/src/test/kotlin/com/tikiticket/ticketsmockapi/TicketsMockapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tikiticket.ticketsmockapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TicketsMockapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
