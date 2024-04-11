7d05d8e137a971ae85d4beb069629f15675072b0
devbyalvina <horannabiii@gmail.com>
2024-04-12 02:14:18 +0900

    API.4 콘서트 스케줄 목록 조회 Error 추가

--- tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/exception/ConcertError.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/exception/ConcertError.kt
@@ -4,5 +4,6 @@ enum class ConcertError (
     val code: String,
     val message: String,
 ) {
-
+    // API.4 콘서트 스케줄 목록 조회
+    CONCERT_NOT_FOUND("CONCERT_NOT_FOUND", "The specified concert was not found."),
 }
