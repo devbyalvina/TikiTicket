7f8a177d89efa19f7075b70988568ba09f9850d6
devbyalvina <horannabiii@gmail.com>
2024-04-12 02:15:19 +0900

    Concert Root Aggregate 생성

---
+++ tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/Concert.kt
@@ -0,0 +1,13 @@
+package com.tikiticket.tickets.concert.domain
+
+import java.time.LocalDateTime
+
+data class Concert(
+    val id: Long,
+    val concertName: String,
+    val artistName: String,
+    val concertDate: LocalDateTime,
+    val venue: String,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+)
