6e5d6497795d06e039744943959c25cd0ef442b7
devbyalvina <horannabiii@gmail.com>
2024-04-12 23:19:43 +0900

    Booking Root Aggregate 생성

---
+++ tickets/src/main/kotlin/com/tikiticket/tickets/booking/domain/Booking.kt
@@ -0,0 +1,31 @@
+package com.tikiticket.tickets.booking.domain
+
+import java.time.LocalDateTime
+
+data class Booking (
+    val id: Long,
+    val bookingStatus: BookingStatusType,
+    val expiryDateTime: LocalDateTime,
+    val concertId: Long,
+    val concertName: String,
+    val artistName: String,
+    val concertDate: LocalDateTime,
+    val venue: String,
+    val searNo: Long,
+    val ticketPrice: Long,
+    val bookedAt: LocalDateTime,
+    val paidAt: LocalDateTime,
+    val expiredAt: LocalDateTime,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+)
+
+/**
+ * 좌석 상태
+ * - BOOKED : 예약
+ * - PAID : 결제완료
+ * - EXPIRED : 만료
+ */
+enum class BookingStatusType {
+    BOOKED, PAID, EXPIRED
+}
