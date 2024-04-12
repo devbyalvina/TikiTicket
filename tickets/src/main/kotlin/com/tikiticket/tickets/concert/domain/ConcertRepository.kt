1cd85a3596766c9db2b0f9b4822d3230c954a71d
devbyalvina <horannabiii@gmail.com>
2024-04-13 03:08:01 +0900

    주석 수정

--- tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/ConcertRepository.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/ConcertRepository.kt
@@ -4,12 +4,12 @@ import java.time.LocalDate
 
 interface ConcertRepository {
     /**
-     *  API.4. 콘서트 스케줄 목록 조회
+     *  콘서트 스케줄 목록 조회
      */
     fun findConcertsByDateRange(startDate: LocalDate, endDate: LocalDate): List<Concert>?
 
     /**
-     *  API.4. 콘서트 좌석 목록 조회
+     *  콘서트 좌석 목록 조회
      */
     fun findConcertWithSeatsById(concertId: Long): Concert?
 }
--- tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/ConcertService.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/concert/domain/ConcertService.kt
@@ -8,7 +8,7 @@ class ConcertService (
     private val concertRepository: ConcertRepository
 ){
     /**
-     *  API.4. 콘서트 스케줄 목록 조회
+     *  콘서트 스케줄 목록 조회
      */
     fun getConcertsByDateRange(startDate: LocalDate, endDate: LocalDate): List<Concert>? {
         val concerts = concertRepository.findConcertsByDateRange(startDate, endDate)
@@ -16,7 +16,7 @@ class ConcertService (
     }
 
     /**
-     *  API.5. 콘서트 좌석 목록 조회
+     *  콘서트 좌석 목록 조회
      */
     fun getConcertWithSeatsById(concertId: Long): Concert? {
         val concert = concertRepository.findConcertWithSeatsById(concertId)
