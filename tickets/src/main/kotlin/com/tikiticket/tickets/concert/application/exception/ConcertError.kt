af4cd2ca998107fa73f6b1a691c21b9bac06fb66
devbyalvina <horannabiii@gmail.com>
2024-04-13 03:47:34 +0900

    코드값 중복 제거

--- tickets/src/main/kotlin/com/tikiticket/tickets/concert/application/exception/ConcertError.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/concert/application/exception/ConcertError.kt
@@ -1,14 +1,22 @@
 package com.tikiticket.tickets.concert.application.exception
 
 enum class ConcertError (
-    val code: String,
     val message: String,
 ) {
-    // API.4 콘서트 스케줄 목록 조회
-    INVALID_DATE_RANGE_PARAMETER("INVALID_DATE_RANGE_PARAMETER", "Invalid date range. Please specify either start_date and end_date or date_range."),
-    // API.4 콘서트 스케줄 목록 조회
-    // API.5 콘서트 좌석 목록 조회
-    CONCERT_NOT_FOUND("CONCERT_NOT_FOUND", "The specified concert was not found."),
-    // API.5 콘서트 좌석 목록 조회
-    CONCERT_SEATS_NOT_FOUND("CONCERT_SEATS_NOT_FOUND", "Seats for the specified concert were not found.")
+    /**
+     *  INVALID_DATE_RANGE_PARAMETER
+     *  - API.4 콘서트 스케줄 목록 조회
+     */
+    INVALID_DATE_RANGE_PARAMETER("Invalid date range. Please specify either start_date and end_date or date_range."),
+    /**
+     *  CONCERT_NOT_FOUND
+     *  - API.4 콘서트 스케줄 목록 조회
+     *  - API.5 콘서트 좌석 목록 조회
+     */
+    CONCERT_NOT_FOUND("The specified concert was not found."),
+    /**
+     *  CONCERT_SEATS_NOT_FOUND
+     *  - API.5 콘서트 좌석 목록 조회
+     */
+    CONCERT_SEATS_NOT_FOUND("Seats for the specified concert were not found.")
 }
