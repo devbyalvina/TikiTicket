a9b9ba693c43cf8ec2e092faf213c4ef52ea5250
devbyalvina <horannabiii@gmail.com>
2024-04-13 03:03:19 +0900

    TicketQueueToken Root Aggregate 생성

---
+++ tickets/src/main/kotlin/com/tikiticket/tickets/ticketqueuetoken/domain/TicketQueueToken.kt
@@ -0,0 +1,21 @@
+package com.tikiticket.tickets.ticketqueuetoken.domain
+
+import java.time.LocalDateTime
+
+data class TicketQueueToken(
+    val id: Long,
+    val userId: String,
+    val tokenStatus: TokenStatusType,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime
+)
+
+/**
+ * 좌석 상태
+ * - WAITING : 대기
+ * - ACTIVE : 활성
+ * - INACTIVE : 비활성
+ */
+enum class TokenStatusType {
+    WAITING, ACTIVE, INACTIVE
+}
