74ce18743d4aa96ee1916901c1408907fd80ef22
devbyalvina <horannabiii@gmail.com>
2024-05-04 05:15:13 +0900

    API.1 대기열 토큰 발급 - CreateTokenUseCase 동시성 통합 테스트 작성

---
+++ tickets/src/test/kotlin/com/tikiticket/tickets/ticketqueuetoken/TicketQueueTokenIntegrationTest.kt
@@ -0,0 +1,41 @@
+package com.tikiticket.tickets.ticketqueuetoken
+
+import com.tikiticket.tickets.ticketqueuetoken.application.CreateTokenUseCase
+import com.tikiticket.tickets.ticketqueuetoken.domain.TicketQueueToken
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.TestConstructor
+import java.time.LocalDateTime
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.atomic.AtomicInteger
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class TicketQueueTokenIntegrationTest (
+    val createTokenUseCase: CreateTokenUseCase
+){
+    @Test
+    fun `대기열 생성 요청이 동시에 들어오는 경우 1개만 성공한다`() {
+        // when
+        val failCount = AtomicInteger(0)
+        val futures = mutableListOf<CompletableFuture<TicketQueueToken>>()
+        repeat(10 ) {
+            futures.add (
+                CompletableFuture.supplyAsync {
+                    runCatching {
+                        createTokenUseCase ("user01", LocalDateTime.now())
+                    }.onFailure {
+                        failCount.incrementAndGet()
+                    }.getOrNull()
+                }
+            )
+        }
+
+        CompletableFuture.allOf(*futures.toTypedArray()).join()
+
+        Assertions.assertEquals(9, failCount.get())
+    }
+}
