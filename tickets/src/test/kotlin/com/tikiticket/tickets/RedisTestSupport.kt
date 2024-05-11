11362b7a001ce3dd2e2ee664352514f284442827
devbyalvina <horannabiii@gmail.com>
2024-05-12 03:48:43 +0900

    [대기열 with Redis Cache] CreateTokenInMemoryUseCase 통합테스트 추가

---
+++ tickets/src/test/kotlin/com/tikiticket/tickets/RedisTestSupport.kt
@@ -0,0 +1,22 @@
+package com.tikiticket.tickets
+
+import com.tikiticket.tickets.global.infrastructure.redis.configuration.LettuceConfiguration
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.context.annotation.Import
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(LettuceConfiguration::class)
+abstract class RedisTestSupport {
+    @Autowired
+    lateinit var redisConnectionFactory: RedisConnectionFactory
+
+    @BeforeEach
+    fun setup() {
+        redisConnectionFactory.connection.commands().flushAll()
+    }
+}
--- tickets/src/test/kotlin/com/tikiticket/tickets/aggregate/ticketqueuetoken/TicketQueueTokenIntegrationTest.kt
+++ tickets/src/test/kotlin/com/tikiticket/tickets/aggregate/ticketqueuetoken/TicketQueueTokenIntegrationTest.kt
@@ -1,7 +1,16 @@
 package com.tikiticket.tickets.aggregate.ticketqueuetoken
 
+import com.tikiticket.tickets.RedisTestSupport
+import com.tikiticket.tickets.aggregate.ticketqueuetoken.application.CreateTokenInMemoryUseCase
+import com.tikiticket.tickets.aggregate.ticketqueuetoken.application.CreateTokenUseCase
+import com.tikiticket.tickets.aggregate.ticketqueuetoken.domain.TicketQueueToken
+import com.tikiticket.tickets.aggregate.ticketqueuetoken.domain.TicketQueueTokenError
+import com.tikiticket.tickets.global.domain.exception.CustomException
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.TestConstructor
@@ -13,13 +22,16 @@ import java.util.concurrent.atomic.AtomicInteger
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 class TicketQueueTokenIntegrationTest (
-    val createTokenUseCase: com.tikiticket.tickets.aggregate.ticketqueuetoken.application.CreateTokenUseCase
-){
+    val createTokenUseCase: CreateTokenUseCase,
+    val createTokenInMemoryUseCase: CreateTokenInMemoryUseCase
+): RedisTestSupport() {
     @Test
     fun `대기열 생성 요청이 동시에 들어오는 경우 1개만 성공한다`() {
-        // when
+        // given
         val failCount = AtomicInteger(0)
-        val futures = mutableListOf<CompletableFuture<com.tikiticket.tickets.aggregate.ticketqueuetoken.domain.TicketQueueToken>>()
+
+        // when
+        val futures = mutableListOf<CompletableFuture<TicketQueueToken>>()
         repeat(10 ) {
             futures.add (
                 CompletableFuture.supplyAsync {
@@ -34,6 +46,30 @@ class TicketQueueTokenIntegrationTest (
 
         CompletableFuture.allOf(*futures.toTypedArray()).join()
 
+        // then
         Assertions.assertEquals(9, failCount.get())
+    }
+
+    private val log: Logger = LoggerFactory.getLogger(TicketQueueTokenIntegrationTest::class.java)
+
+    @Test
+    fun `메모리 저장소에 대기열 토큰 생성 요청 시 토큰 생성에 성공한다`() {
+        // When
+        val result = createTokenInMemoryUseCase("user01")
+
+        // Then
+        assertNotNull(result)
+    }
+
+    @Test
+    fun `메모리 저장소에 대기열 토큰이 이미 있는 경우 토큰 생성 요청 시 예외를 반환한다`() {
+        // When
+        val result = createTokenInMemoryUseCase("user01")
+        val exception = Assertions.assertThrows(CustomException::class.java) {
+            createTokenInMemoryUseCase("user01")
+        }
+
+        // Then
+        Assertions.assertEquals(TicketQueueTokenError.TOKEN_ALREADY_ISSUED, exception.customError)
     }
 }
