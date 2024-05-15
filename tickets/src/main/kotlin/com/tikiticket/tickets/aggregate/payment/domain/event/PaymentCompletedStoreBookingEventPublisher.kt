498c1a72c4d3c2379190dd2124f946abdb805e69
devbyalvina <horannabiii@gmail.com>
2024-05-15 16:50:59 +0900

    결제 완료 EventPublisher @Component 추가

--- tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/payment/domain/event/PaymentCompletedStoreBookingEventPublisher.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/payment/domain/event/PaymentCompletedStoreBookingEventPublisher.kt
@@ -1,10 +1,12 @@
 package com.tikiticket.tickets.aggregate.payment.domain.event
 
 import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Component
 
 /**
  * 결제 완료 시 예매 내역 저장 이벤트
  */
+@Component
 class PaymentCompletedStoreBookingEventPublisher (
     private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
