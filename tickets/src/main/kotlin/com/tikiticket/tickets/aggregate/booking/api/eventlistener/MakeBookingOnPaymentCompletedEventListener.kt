017dec5ea2e98ea316e93d6d50dabb88bac23ec1
devbyalvina <horannabiii@gmail.com>
2024-05-17 03:10:51 +0900

    결제 완료 EventListener 변경 (Transacitonal Outbox 패턴을 사용하기 위함)

--- tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/booking/api/eventlistener/MakeBookingOnPaymentCompletedEventListener.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/booking/api/eventlistener/MakeBookingOnPaymentCompletedEventListener.kt
@@ -1,7 +1,7 @@
-package com.tikiticket.tickets.aggregate.booking.application.eventhandler
+package com.tikiticket.tickets.aggregate.booking.api.eventlistener
 
-import com.tikiticket.tickets.aggregate.booking.domain.BookingService
-import com.tikiticket.tickets.aggregate.payment.domain.event.PaymentCompletedStoreBookingEvent
+import com.tikiticket.tickets.aggregate.booking.application.MakeBookingInMemoryUseCase
+import com.tikiticket.tickets.aggregate.payment.domain.event.PaymentCompletedEvent
 import org.springframework.scheduling.annotation.Async
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -9,12 +9,12 @@ import org.springframework.transaction.event.TransactionalEventListener
 /**
  * 결제 완료 시 예매 내역 저장 이벤트 리스너
  */
-class StoreBookingOnPaymentCompletedEventListener (
-    private val bookingService: BookingService
+class MakeBookingOnPaymentCompletedEventListener (
+    private val makeBookingInMemoryUseCase: MakeBookingInMemoryUseCase
 ) {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-    operator fun invoke(event: PaymentCompletedStoreBookingEvent) {
+    operator fun invoke(event: PaymentCompletedEvent) {
         // 임시 예약 내용을 저장소에 저장하는 서비스 로직
     }
 }
---
+++ tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/booking/application/MakeBookingInMemoryUseCase.kt
@@ -0,0 +1,10 @@
+package com.tikiticket.tickets.aggregate.booking.application
+
+import org.springframework.stereotype.Component
+
+/**
+ *  API.6 예매 - Redis Cache에 저장
+ */
+@Component
+class MakeBookingInMemoryUseCase {
+}
