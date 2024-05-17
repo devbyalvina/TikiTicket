efb288c7cb87ffac2bb109a52e328db4f9a326db
devbyalvina <horannabiii@gmail.com>
2024-05-18 00:26:31 +0900

    사용하지 않는 Event, EventPublisher, EventListener 삭제

--- tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/payment/domain/event/PaymentCompletedChangeConcertEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.tikiticket.tickets.aggregate.payment.domain.event
-
-data class PaymentCompletedChangeConcertEvent (
-    val paymentId: Long,
-    val concertId: Long,
-    val concertSeatId: Long,
-)
--- tickets/src/main/kotlin/com/tikiticket/tickets/aggregate/payment/domain/event/PaymentCompletedStoreBookingEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.tikiticket.tickets.aggregate.payment.domain.event
-
-data class PaymentCompletedStoreBookingEvent(
-    val paymentId: Long,
-    val concertId: Long,
-    val concertSeatId: Long,
-    val userId: String,
-)
