e7885c1cc53988c105d283503fa53ef5abde26ad
devbyalvina <horannabiii@gmail.com>
2024-05-19 23:52:31 +0900

    스프링 메모리 캐시 레디스에 저장하도록 설정 변경

--- tickets/src/main/kotlin/com/tikiticket/tickets/global/infrastructure/redis/configuration/LettuceConfig.kt
+++ tickets/src/main/kotlin/com/tikiticket/tickets/global/infrastructure/redis/configuration/LettuceConfig.kt
@@ -1,13 +1,25 @@
 package com.tikiticket.tickets.global.infrastructure.redis.configuration
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.tikiticket.tickets.aggregate.concert.domain.ConcertTest
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
 
 
+@EnableCaching
 @Configuration
 class LettuceConfig {
 //    @Bean
@@ -22,5 +34,26 @@ class LettuceConfig {
         redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer()
         redisTemplate.connectionFactory = connectionFactory
         return redisTemplate
+    }
+
+    @Bean(name = arrayOf("redisCacheManager"))
+    fun cacheManager(connectionFactory: RedisConnectionFactory): CacheManager {
+        val builder = RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+        val configuration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(Jackson2JsonRedisSerializer(objectMapper(), ConcertTest::class.java)))
+            .entryTtl(Duration.ofMinutes(30))
+        builder.cacheDefaults(configuration)
+        return builder.build()
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper().apply {
+            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            registerModule(JavaTimeModule())
+        }
+        return objectMapper
     }
 }
